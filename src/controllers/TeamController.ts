acbda530cb8aef20d9914bf47a4ceac3d17362f8
Adrian Rivarola <ad.rivarola18@gmail.com>
2026-07-13 02:06:05 -0500

    static notifications project fixed

--- src/controllers/TeamController.ts
+++ src/controllers/TeamController.ts
@@ -1,6 +1,8 @@
 import type { Request, Response } from 'express'
 import User from '../models/UserModel'
 import Project from '../models/ProjectModel'
+import { notifyChangesToTeam } from '../services/notificationService'
+import { Types } from 'mongoose'
 
 
 export class TeamMemberController {
@@ -42,20 +44,42 @@ export class TeamMemberController {
         req.project.team.push(user._id)
         await req.project.save()
 
+        const members = [{_id: _id}]
+
+        await notifyChangesToTeam({
+                members: members as Array<{ _id: Types.ObjectId }>,
+                triggeredBy: req.user!._id!,
+                projectId: req.project._id,
+                taskId: null, // No hay una tarea específica asociada a esta notificación
+                actionType: "MEMBER_ADDED",
+                content: `${req.user!.name} te agrego al proyecto "${req.project.projectName}"`,
+              });
+
         res.send('Usuario agregado correctamente')
     }
 
     static removeMemberById = async (req: Request, res: Response) => {
-        const { userID } = req.params
+        const _id  = req.params.userID! as string;
         
-        if(!req.project.team.some(team => team?.toString() ===  userID)) {
+        if(!req.project.team.some(team => team?.toString() === _id)) {
             const error = new Error('El usuario no existe en el proyecto')
             return res.status(409).json({error: error.message})
         }
 
-        req.project.team = req.project.team.filter( teamMember => teamMember?.toString() !==  userID)
+        req.project.team = req.project.team.filter( teamMember => teamMember?.toString() !== _id)
         await req.project.save()
+
+        const members = [{_id: new Types.ObjectId(_id)}]
+
+        await notifyChangesToTeam({
+                members: members as Array<{ _id: Types.ObjectId }>,
+                triggeredBy: req.user!._id!,
+                projectId: req.project._id,
+                taskId: null, // No hay una tarea específica asociada a esta notificación
+                actionType: "MEMBER_REMOVED",
+                content: `${req.user!.name} te elimino del proyecto "${req.project.projectName}"`,
+              });
         
-        res.json({message: 'Usuario eliminado correctamente', manager:req.user?.name, colaborador: userID})
+        res.json({message: 'Usuario eliminado correctamente', manager:req.user?.name, colaborador: _id})
     }
 }
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -17,42 +17,42 @@ export const setupSocket = (io: Server) => {
 
     socket.on("project_deleted", (data) => {
       data.team.forEach((memberID: string) => {
-        socket.to(memberID).emit("project_deleted_notification", data);
+        socket.to(memberID).emit("project_deleted_notification", data.message);
       });
     });
 
     //socket members event
     socket.on('member_added', (data) => {
-      socket.to(data.userID).emit("member_added_notification", data)
+      socket.to(data.userID).emit("member_added_notification", data.message)
     });
 
     socket.on('member_removed', (data) => {
-      socket.to(data.userID).emit("member_removed_notification", data)
+      socket.to(data.userID).emit("member_removed_notification", data.message)
     })
 
     //socket task events
     socket.on("task_created", (data) => {
       data.project.team.forEach((memberID: string) => {
-        socket.to(memberID).emit("task_created_notification", {projectID: data.project._id});
+        socket.to(memberID).emit("task_created_notification", {message: data.message,projectID: data.project._id});
       });
     });
 
     socket.on("taskDeleted", (data) => {
       data.project.team.forEach((memberID: string) => {
-        socket.to(memberID).emit("task_deleted_notification", {projectID: data.project._id});
+        socket.to(memberID).emit("task_deleted_notification", {message: data.message,projectID: data.project._id});
       });
   });
 
     socket.on("taskUpdated", (data) => {
       data.project.team.forEach((memberID: string) => {
-        socket.to(memberID).emit("task_updated_notification", {projectID: data.project._id});
+        socket.to(memberID).emit("task_updated_notification", {message: data.message,projectID: data.project._id});
       });
     }) 
 
     socket.on("task_status_update", (data) => {
       data.team.forEach((memberID: string) => {
         if(memberID !== data.triggeredBy) { // excluye al emisor
-            socket.to(memberID).emit("task_status_updated_notification", {projectID: data.project._id})
+            socket.to(memberID).emit("task_status_updated_notification", {message: data.message, projectID: data.projectID})
         }
       }); // solo a la sala del proyecto
     });
