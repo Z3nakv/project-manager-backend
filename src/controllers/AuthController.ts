98f5d1a19eb56fa54d27e3be7c6d735665caf87c
Adrian Rivarola <ad.rivarola18@gmail.com>
2026-08-12 14:14:28 -0500

    googleauth strict fixed

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -216,7 +216,7 @@ export class AuthController {
       res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000
       })
       res.json({ user, accessToken });
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -5,13 +5,6 @@ type UserPayload = {
     id: Types.ObjectId
 }
 
-
-/* export const generateJWT = (payload: UserPayload) => {
-    const jwtSecret = process.env.JWT_SECRET!
-    const token = jwt.sign(payload, jwtSecret, {expiresIn: '180d'});
-    return token;
-} */
-
 export const generateAccessToken = (payload: UserPayload): string => {
   const jwtSecret = process.env.JWT_SECRET!;
   return jwt.sign(payload, jwtSecret, { expiresIn: "15m" });
